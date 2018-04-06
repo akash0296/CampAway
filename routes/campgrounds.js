34117ef1185dcb8c0da2d3ba658e241cc22ba006
Akash Mukherjee <akashmukherjee452@gmail.com>
2018-04-06 16:26:55 +0000

    Added code for dynamic pricing of campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -29,11 +29,12 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
+    var price = req.body.price;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = {name: name, image: image, description: description, author: author};
+    var newCampground = {name: name, image: image, description: description, author: author, price: price};
     //CREATING NEW CAMPGROUND.
     Campground.create(newCampground, function(error, newlyCreated){
         if(error){
